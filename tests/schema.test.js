9a20b59baeb786e8b59b4fe24160967461e0d21b
Nasenboi <c.boendgen@gmail.com>
2025-03-11 09:19:19 +0100

    less output for schema tests

--- tests/schema.test.js
+++ tests/schema.test.js
@@ -9,9 +9,9 @@ describe("Validate exampleInvoice", () => {
     EInvoiceSchema.validate(exampleInvoice);
     var invoice = EInvoiceSchema.clean(exampleInvoice);
     invoice = addXmlData("EInvoiceSchema", invoice);
-    console.log("xml data", JSON.stringify(addXmlData("EInvoiceSchema", invoice), null, 2));
+
     const xml = eInvoiceToXml(invoice).xml;
-    //console.log("xml", xml);
+    
     // store under tests/text-objects/example-invoice.xml
     const path = "./tests/test-objects/example-invoice.xml";
     fs.writeFileSync(path, xml);
